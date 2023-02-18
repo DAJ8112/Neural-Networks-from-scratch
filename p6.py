d1592c2d38f580d8969fb15cfb52a7d83eb9aea0
Dhruvil Joshi <daj8112@gmail.com>
2023-02-18 11:47:30 +0530

    6th code for ReLU

--- p6.py
+++ p6.py
@@ -1,12 +1,32 @@
-# OOP approach to layers 
-
 import numpy as np
-np.random.seed(0)
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
+
+
 
 X = [[1, 2, 3, 2.5], # this could be the raw actual data and the first row is the first sample . No of features/attributes per sample here = 4
 	[2.0, 5.0, -1.0, 2.0],
 	[-1.5, 2.7, 3.3, -0.8]]
 
+
+X, y = spiral_data(100, 3)
+
+"""
+ReLU simple execution
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+for i in inputs:
+if i > 0:
+	output.append(i)
+elif i <= 0:
+	output.append(0)
+	OR
+	output.append(max(0, i ))
+
+print(output)
+"""
 
 class Layer_Dense:
 
@@ -17,10 +37,15 @@ class Layer_Dense:
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights)+ self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2) # 5 here because it has it's inputs from previous layer but the no of neurons could be any number
+
+class Activation_ReLU:
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output) 
+# print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
